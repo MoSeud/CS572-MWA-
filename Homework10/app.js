68e36fc67770f5bc14dc1382663da9d2f97b4e96
Mohammed Seud <mseud52@gmail.com>
2019-04-15 09:56:35 -0500

    homework 10

--- Homework10/app.js
+++ Homework10/app.js
@@ -6,7 +6,20 @@ var university = /** @class */ (function () {
     university.prototype.graduation = function (year) {
         console.log("Graduating " + this.dept + " " + year + " students");
     };
+    university.prototype.getName = function () {
+        return this.name;
+    };
+    university.prototype.setName = function (name) {
+        this.name = name;
+    };
+    university.prototype.getDept = function () {
+        return this.dept;
+    };
+    university.prototype.setDept = function (dept) {
+        this.dept = dept;
+    };
     return university;
 }());
 var mum = new university("MUM", "CS");
+mum.setDept("MUMcompro");
 mum.graduation(2019);
--- Homework10/app.ts
+++ Homework10/app.ts
@@ -6,7 +6,20 @@ class university {
     graduation(year:number){
   console.log(`Graduating ${ this.dept} ${year} students`)
     }
+    getName(){
+      return this.name;
+    }
+    setName(name){
+        this.name=name;
+    }
+    getDept(){
+      return this.dept;
+    }
+    setDept(dept){
+      this.dept=dept;
+    }
 }
 
 let mum=new university("MUM","CS");
+mum.setDept("MUMcompro");
 mum.graduation(2019);
